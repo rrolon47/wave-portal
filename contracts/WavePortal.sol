8b0af624a02f9555716f3388370b654f487c8c55
rrolon47 <rachel.rolon@gmail.com>
2021-12-26 23:50:08 -0600

    adding waves, then messing around with the code

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -7,8 +7,23 @@ import "hardhat/console.sol";
 //hardhat allowing us to use consol.logs for debuggin
 
 contract WavePortal {
+    uint256 totalBirds;
+    //this is a "state variable" stored permanetly in contract storage.
+
     constructor() {
         console.log("Smarty McSmartContract!");
     }
+
+    function bird() public {
+        totalBirds += 1;
+        console.log("%s has flipped you the bird!", msg.sender);
+        //<msg.sender> wallet address of the person who clled the function.  Like built in authenticaton.  to beable to call a smart contrat function you need to be connecte with a valid wallet.  we can later write function that only allow certain wallets addresses can hit. 
+    }
+
+    function getTotalBirds() public view returns (uint256) {
+        console.log("Smarty McSmartContract has been flipped the bird %d times!", totalBirds);
+        return totalBirds;
+    }
+
     //this is a smart contract. it kinda looks like a class
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,16 +1,41 @@
+
 const main = async () => {
-    const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
+    const [owner, randomPerson] = await hre.ethers.getSigners();
+    //gets contract owner wallet address and other persons wallet address (potential waver)
+    
+    const birdContractFactory = await hre.ethers.getContractFactory('WavePortal');
     //compiles our contract
     //creates files used to work with contract in /artifacts
     //hre stands for Hardhat Runtime Environment: an object containing all functionality of Hardhat when running a task, test, or script. 
-    const waveContract = await waveContractFactory.deploy();
+    const birdContract = await birdContractFactory.deploy();
     //this makes hardhat create a local ethereum network(blockchain) to run this contract locally
     //it is destroyed after it runs (kinda like when you run a app locally with out persistent DB)
-    await waveContract.deployed();
+    await birdContract.deployed();
     //this waits for the local blockchain to be deployed before actually running the constructor.
-    console.log("Contract deployed to:", waveContract.address);
+    
+    console.log("Find this contract at:", birdContract.address);
     //this gives us the address of the deployed contract so we can find the contract on the blockchain
+    console.log("Deployed by Smarty McSmartContract:", owner.address);
+    //gets the address of the person deploying our contract
+
+    //here we manually call our functions made in WavePortal.sol like an API call  
+    let birdCount;
+    birdCount = await birdContract.getTotalBirds();
+    //see how many waves we have
+    
+    let birdTxn = await birdContract.bird();
+    await birdTxn.wait();
+    //do the wave (changes the state variable)
+    
+    birdCount = await birdContract.getTotalBirds();
+    //get the number of waves to see it it changed
+
+    birdTxn = await birdContract.connect(randomPerson).bird();
+    await birdTxn.wait();
+
+    birdCount = await birdContract.getTotalBirds();
   };
+
   
   const runMain = async () => {
     try {
